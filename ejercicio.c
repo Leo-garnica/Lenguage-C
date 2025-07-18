56dad1dc9172fcd75ff5ce64aa1414291d40710e
Leo-garnica <leo123leonardo907@gmail.com>
2025-07-17 23:51:10 -0400

    Ejercicios

---
+++ ejercicio.c
@@ -0,0 +1,25 @@
+﻿#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    pid_t pid;
+    int status;
+
+    pid = fork();
+    if (pid == 0) {
+        sleep(5);
+        exit(5);
+    }
+
+    pid = fork();
+    if (pid == 0) {
+        sleep(1);
+        exit(1);
+    }
+
+    wait(&status);
+    wait(&status);
+}
